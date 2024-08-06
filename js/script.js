0435201710800f0520a751aa32dee261d4462c94
Omkar Jahagirdar <omkar3602@gmail.com>
2024-08-06 17:16:58 +0530

    UI changes

--- js/script.js
+++ js/script.js
@@ -26,3 +26,19 @@ tabs.forEach(tab => {
 
     })
 });
+
+// Show or hide the button based on scroll position
+window.onscroll = function() {
+    var button = document.getElementById('back-to-top');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+};
+
+// Scroll to the top of the document
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
