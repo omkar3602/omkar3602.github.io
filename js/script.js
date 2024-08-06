a0ac00d8c8dfe02989a287264fac4c5610000d0c
Omkar Jahagirdar <omkar3602@gmail.com>
2024-08-07 01:10:10 +0530

    Add Smooth transition

--- js/script.js
+++ js/script.js
@@ -39,6 +39,5 @@ window.onscroll = function() {
 
 // Scroll to the top of the document
 function scrollToTop() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
